94250d2034820a373e473202f7dd53e199726e66
jiayingxie <xie.jiay@northeastern.edu>
2022-09-23 20:39:43 -0700

    Updated contents

--- src/components/ActionPage/ActionPageBody/ActionPageBody.js
+++ src/components/ActionPage/ActionPageBody/ActionPageBody.js
@@ -3,101 +3,117 @@ import styles from "./ActionPageBody.module.scss";
 import { Row, Container } from "reactstrap";
 
 export default class ActionPageBody extends Component {
-  render() {
-    return (
-      <div className={styles.ActionPageBody}>
-        <Container id="awareness">
-          <Row>
-            <h1>Raise awareness</h1>
-          </Row>
-          <ul>
-            <li>
-              Tell a friend or colleague that you are concerned about going on
-              the street nowadays because of anti-Asian incidents.
-            </li>
-            <li>Having 1-on-1 with coworkers to talk about the issue.</li>
-            <li>
-              In a team meeting, share with colleagues what you are doing to
-              fight hate.
-            </li>
-            <li>Share anti-hate materials to your team or organization.</li>
-            <li>
-              If you experienced a biased incident, tell your non-asian friends
-              and colleagues about that. Share both facts and your feelings.
-            </li>
-            <li>
-              Raise questions to the leadership of your organization, encourage
-              concrete plans to address the problems.
-            </li>
-            <li>
-              Inform your asian/non-asian friends about actions taken by
-              governments and companies. Knowing that anti-Asian hate has drawn
-              significant society-wise attention (not just among AAPI
-              communities) will encourage everyone to do something.
-            </li>
-          </ul>
-        </Container>
-        <Container id="support">
-          <Row>
-            <h1>Support anti-Asian hate victims</h1>
-          </Row>
-          <ul>
-            <li>Donate</li>
-            <li>Help capture suspects</li>
-            <li>Help effort to bring justice to victims</li>
-            <li>
-              Tell a hate incident/crime victim that you want to listen to their
-              feelings.
-            </li>
-            <li>
-              Tell a hate incident/crime victim that you are here to help.
-            </li>
-          </ul>
-        </Container>
-        <Container id="expertise">
-          <Row>
-            <h1>Use your profession & expertise to help</h1>
-          </Row>
-          <ul>
-            <li>
-              Make an art piece that reflects your attitude to asian-hate and
-              bias
-            </li>
-            <li>
-              Create articles, arts, paintings, videos, music to advocate social
-              equality. Or simply share those artifacts you found helpful.
-            </li>
-            <li>Develop or share apps, websites.</li>
-            <li>Help proofread articles, slides that other people created.</li>
-            <li>Help improve UX for artifacts that other people created.</li>
-            <li>
-              Help fact-check, data crawling, data analysis, data visualization.
-            </li>
-            <li>Propose or participate projects.</li>
-          </ul>
-        </Container>
-        <Container id="culture">
-          <Row>
-            <h1>Promote cross racial understanding</h1>
-          </Row>
-          <ul>
-            <li>
-              Ask a minority friend about challenges they experienced while
-              growing up and living in the US. Try to relate to them and perhaps
-              share your challenges with what the AAPI community is facing.
-            </li>
-            <li>
-              Invite friends from different ethnic groups to join your own
-              ethnic group or team to tell their stories.
-            </li>
-            <li>
-              Recommend Asian storytelling movies. eg: Crazy Rich Asians, The
-              Joy Luck Club, etc., to friends & colleagues
-            </li>
-            <li>Share something fun about Asian to your non-Asian friends.</li>
-          </ul>
-        </Container>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className={styles.ActionPageBody}>
+				<Container id="awareness">
+					<Row>
+						<h1>Raise awareness</h1>
+					</Row>
+					<ul>
+						<li>
+							Tell a friend or colleague that you are concerned for your safety
+							out in the streets nowadays because of Anti-Asian hate crime
+							incidents.
+						</li>
+						<li>Talk about this issue with coworkers in 1-on-1 meetings.</li>
+						<li>In a team meeting, share what you are doing to fight hate.</li>
+						<li>Share anti-hate materials to your team or organization.</li>
+						<li>
+							If you experienced a biased incident, tell your non-asian friends
+							and colleagues about that. Share both facts and your feelings.
+						</li>
+						<li>
+							Raise questions to the leaders of your organization, encourage
+							concrete plans to address this issue.
+						</li>
+						<li>
+							Inform your asian/non-asian friends about actions taken by
+							governments and companies. Knowing that anti-Asian hate has drawn
+							significant society-wise attention (not just among AAPI
+							communities) will encourage everyone to do something.
+						</li>
+					</ul>
+				</Container>
+				<Container id="support">
+					<Row>
+						<h1>Support anti-Asian hate victims</h1>
+					</Row>
+					<ul>
+						<li>Donate</li>
+						<li>Help capture suspects</li>
+						<li>Help effort to bring justice to victims</li>
+						<li>
+							Tell a hate incident/crime victim that you want to listen to their
+							feelings.
+						</li>
+						<li>
+							Tell a hate incident/crime victim that you are here to help.
+						</li>
+					</ul>
+				</Container>
+				<Container id="expertise">
+					<Row>
+						<h1>
+							Use your professional skills & expertise to make a difference
+						</h1>
+					</Row>
+					<ul>
+						<li>
+							Create: paint, draw, or digitally make a piece of art that
+							reflects your attitude towards asian-hate, prejudice, and bias
+						</li>
+						<li>
+							Create articles, arts, paintings, videos, music to advocate social
+							equality. Or simply share those artifacts you found helpful.
+						</li>
+						<li>Share: art, websites, or books that inspire and help you</li>
+						<li>
+							Support: use your analytical skills to proofread articles, slide
+							decks, and other educational content
+						</li>
+						<li>
+							Research: fact-check news articles, perform data analysis, or
+							implement data crawling
+						</li>
+						<li>
+							Research: fact-check news articles, perform data analysis, or
+							implement data crawling
+						</li>
+						<li>
+							Collaborate: propose solutions, help with projects, or volunteer
+							out in the community
+						</li>
+					</ul>
+				</Container>
+				<Container id="culture">
+					<Row>
+						<h1>Promote cross racial understanding</h1>
+					</Row>
+					<ul>
+						<li>
+							Communicate: as a minority friend about their experience living in
+							the US
+						</li>
+						<li>
+							Empathize: try to relate to others, share the challenges the APPI
+							community is facing
+						</li>
+						<li>
+							Connect: host inclusive gatherings to connect with friends from
+							different ethnic groups to
+						</li>
+						<li>
+							Recommend: Asian storytelling movies. Eg: Crazy Rich Asians, The
+							Joy Luck Club, to friends & colleagues
+						</li>
+						<li>
+							Invite: share something fun about Asian culture with non-Asian
+							friends
+						</li>
+					</ul>
+				</Container>
+			</div>
+		);
+	}
 }
--- src/components/HCTChart/ChartView.js
+++ src/components/HCTChart/ChartView.js
@@ -14,187 +14,186 @@ import * as incidentsService from "../../services/incidents";
 // import { useRouter } from '../../utility/hooks/useRouter';
 
 const ChartView = () => {
-  // const router = useRouter();
-  const [dateRange, setDateRange] = useState([
-    moment().subtract(1, "year"),
-    moment(),
-  ]);
-  const [isFirstLoadData, setIsFirstLoadData] = useState(true);
-  const [totalAnnualCases, setTotalCases] = useState(0);
-  const [totalLastMonthCases, setTotalLastMonthCases] = useState(0);
-  const [lastMonthName, setLastMonthName] = useState("");
-  const [incidentTimeSeries, setIncidentTimeSeries] = useState([
-    {
-      monthly_cases: 0,
-      key: moment().format("YYYY-MM-DD"),
-      value: 0,
-    },
-  ]);
-  const [loading, setLoading] = useState(false);
+	// const router = useRouter();
+	const [dateRange, setDateRange] = useState([
+		moment().subtract(1, "year"),
+		moment(),
+	]);
+	const [isFirstLoadData, setIsFirstLoadData] = useState(true);
+	const [totalAnnualCases, setTotalCases] = useState(0);
+	const [totalLastMonthCases, setTotalLastMonthCases] = useState(0);
+	const [lastMonthName, setLastMonthName] = useState("");
+	const [incidentTimeSeries, setIncidentTimeSeries] = useState([
+		{
+			monthly_cases: 0,
+			key: moment().format("YYYY-MM-DD"),
+			value: 0,
+		},
+	]);
+	const [loading, setLoading] = useState(false);
 
-  // stats [{'2021-01-02:1}, {'2021-01-01:1}...]  dates descending
-  // Remove date out of the range, and insert days that does not have data
-  // start_date, end_date: Date
-  // monthly: monthly aggregation { first_day_of_month: count_of_the_month }
-  const mergeDate = (stats, start_date, end_date, monthly) => {
-    const new_stats = [];
-    let start = moment(start_date);
-    const end = moment(end_date);
-    const strStartDate = start.format("YYYY-MM-DD");
-    const strEndDate = end.format("YYYY-MM-DD");
-    while (start <= end) {
-      const strDate = start.format("YYYY-MM-DD");
-      const monthlyData = monthly[start.format("YYYY-MM")];
-      if (stats.length > 0) {
-        if (
-          stats[stats.length - 1].key < strStartDate ||
-          stats[stats.length - 1].key > strEndDate
-        ) {
-          stats.pop();
-          continue; //skip data that is out of range
-        }
-        if (stats[stats.length - 1].key === strDate) {
-          //found the date in stats, use it
-          new_stats.push({
-            monthly_cases: monthlyData,
-            ...stats[stats.length - 1],
-          });
-          stats.pop();
-          continue;
-        }
-      }
-      new_stats.push({ key: strDate, value: null, monthly_cases: monthlyData });
-      start.add(1, "days");
-    }
-    return new_stats;
-  };
-  const calculateTotalCases = (stats) => {
-    let total = 0;
-    stats.forEach((stat) => {
-      total += stat.value;
-    });
-    return total;
-  };
-  const calculateTotalCasesOfMonth = (stats, month /* string 2022-01 */) => {
-    let total = 0;
-    stats.forEach((stat) => {
-      if (stat.key.substring(0, 7) === month) {
-        total += stat.value;
-      }
-    });
-    return total;
-  };
-  const loadData = () => {
-    setLoading(true);
-    incidentsService.getStats(dateRange[0], dateRange[1]).then((stats) => {
-      const mergedData = mergeDate(
-        stats.stats,
-        dateRange[0],
-        dateRange[1],
-        stats.monthly_stats
-      );
-      setIncidentTimeSeries(mergedData);
-      setTotalCases(calculateTotalCases(mergedData));
-      const lastMonthName = moment().subtract(1, "month").format("MMMM");
-      const lastMonth = moment().subtract(1, "month").format("YYYY-MM");
-      setTotalLastMonthCases(calculateTotalCasesOfMonth(mergedData, lastMonth));
-      setLastMonthName(lastMonthName);
-      setLoading(false);
-      setIsFirstLoadData(false);
-    });
-  };
+	// stats [{'2021-01-02:1}, {'2021-01-01:1}...]  dates descending
+	// Remove date out of the range, and insert days that does not have data
+	// start_date, end_date: Date
+	// monthly: monthly aggregation { first_day_of_month: count_of_the_month }
+	const mergeDate = (stats, start_date, end_date, monthly) => {
+		const new_stats = [];
+		let start = moment(start_date);
+		const end = moment(end_date);
+		const strStartDate = start.format("YYYY-MM-DD");
+		const strEndDate = end.format("YYYY-MM-DD");
+		while (start <= end) {
+			const strDate = start.format("YYYY-MM-DD");
+			const monthlyData = monthly[start.format("YYYY-MM")];
+			if (stats.length > 0) {
+				if (
+					stats[stats.length - 1].key < strStartDate ||
+					stats[stats.length - 1].key > strEndDate
+				) {
+					stats.pop();
+					continue; //skip data that is out of range
+				}
+				if (stats[stats.length - 1].key === strDate) {
+					//found the date in stats, use it
+					new_stats.push({
+						monthly_cases: monthlyData,
+						...stats[stats.length - 1],
+					});
+					stats.pop();
+					continue;
+				}
+			}
+			new_stats.push({ key: strDate, value: null, monthly_cases: monthlyData });
+			start.add(1, "days");
+		}
+		return new_stats;
+	};
+	const calculateTotalCases = stats => {
+		let total = 0;
+		stats.forEach(stat => {
+			total += stat.value;
+		});
+		return total;
+	};
+	const calculateTotalCasesOfMonth = (stats, month /* string 2022-01 */) => {
+		let total = 0;
+		stats.forEach(stat => {
+			if (stat.key.substring(0, 7) === month) {
+				total += stat.value;
+			}
+		});
+		return total;
+	};
+	const loadData = () => {
+		setLoading(true);
+		incidentsService.getStats(dateRange[0], dateRange[1]).then(stats => {
+			const mergedData = mergeDate(
+				stats.stats,
+				dateRange[0],
+				dateRange[1],
+				stats.monthly_stats
+			);
+			setIncidentTimeSeries(mergedData);
+			setTotalCases(calculateTotalCases(mergedData));
+			const lastMonthName = moment().subtract(1, "month").format("MMMM");
+			const lastMonth = moment().subtract(1, "month").format("YYYY-MM");
+			setTotalLastMonthCases(calculateTotalCasesOfMonth(mergedData, lastMonth));
+			setLastMonthName(lastMonthName);
+			setLoading(false);
+			setIsFirstLoadData(false);
+		});
+	};
 
-  useEffect(() => {
-    loadData();
-  }, []);
+	useEffect(() => {
+		loadData();
+	}, []);
 
-  //responsive
-  const [dimensions, setDimensions] = React.useState({
-    height: window.innerHeight,
-    width: window.innerWidth,
-  });
-  const handleResize = () => {
-    setDimensions({
-      height: window.innerHeight,
-      width: window.innerWidth,
-    });
-  };
-  React.useEffect(() => {
-    window.addEventListener("resize", handleResize);
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  });
+	//responsive
+	const [dimensions, setDimensions] = React.useState({
+		height: window.innerHeight,
+		width: window.innerWidth,
+	});
+	const handleResize = () => {
+		setDimensions({
+			height: window.innerHeight,
+			width: window.innerWidth,
+		});
+	};
+	React.useEffect(() => {
+		window.addEventListener("resize", handleResize);
+		return () => {
+			window.removeEventListener("resize", handleResize);
+		};
+	});
 
-  return (
-    <div className="hcr">
-      <div className="title">
-        <p className="t3">Anti-Asian hate crimes are happening everyday!</p>
-        <p className="t4">
-          Based on incidents reported by major media in the US
-        </p>
-      </div>
+	return (
+		<div className="hcr">
+			<div className="title">
+				<p className="t3">Anti-Asian hate crimes are happening everyday.</p>
+				<p className="t4">
+					The following numbers are based on incidents reported by major media
+					in the US
+				</p>
+			</div>
 
-      {dimensions.width < 576 ? (
-        <div>
-          <Container className="word">
-            <div className="childword">
-              <p className="t2">{totalLastMonthCases} Cases</p>
-              <p className="t1">was reported in {lastMonthName}</p>
-            </div>
+			{dimensions.width < 576 ? (
+				<div>
+					<Container className="word">
+						<div className="childword">
+							<p className="t2">{totalLastMonthCases} Cases</p>
+							<p className="t1">were reported in {lastMonthName}</p>
+						</div>
 
-            <div>
-              <p className="t2">{totalAnnualCases} Cases</p>
-              <p className="t1">was reported last year</p>
-            </div>
-          </Container>
-        </div>
-      ) : (
-        <div>
-          <Container className="word">
-            <Row className="childword justify-content-center">
-              <Col className="col-3">
-                <h2 className="t2">{totalLastMonthCases} Cases</h2>
-                <p className="t1">was reported in {lastMonthName}</p>
-              </Col>
+						<div>
+							<p className="t2">{totalAnnualCases} Cases</p>
+							<p className="t1">were reported last year</p>
+						</div>
+					</Container>
+				</div>
+			) : (
+				<div>
+					<Container className="word">
+						<Row className="childword justify-content-center">
+							<Col className="col-3">
+								<h2 className="t2">{totalLastMonthCases} Cases</h2>
+								<p className="t1">was reported in {lastMonthName}</p>
+							</Col>
 
-              <Col className="col-3">
-                <h2 className="t2">{totalAnnualCases} Cases</h2>
-                <p className="t1">was reported last year</p>
-              </Col>
-            </Row>
-          </Container>
-        </div>
-      )}
-      <div className="chart">
-        <IncidentChart
-          className="behind-relative"
-          chart_data={incidentTimeSeries}
-          isFirstLoadData={isFirstLoadData}
-        />
-      </div>
+							<Col className="col-3">
+								<h2 className="t2">{totalAnnualCases} Cases</h2>
+								<p className="t1">was reported last year</p>
+							</Col>
+						</Row>
+					</Container>
+				</div>
+			)}
+			<div className="chart">
+				<IncidentChart
+					className="behind-relative"
+					chart_data={incidentTimeSeries}
+					isFirstLoadData={isFirstLoadData}
+				/>
+			</div>
 
-      <div>
-        <Row className="link1">
-          <Col
-            lg={{ offset: 10 }}
-            md={{ offset: 8 }}
-            sm={{ offset: 5 }}
-            xs={{ offset: 5 }}
-          >
-            <a
-              className="vm1"
-              href="https://hatecrimetracker.1thing.org/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              View More &rarr;
-            </a>
-          </Col>
-        </Row>
-      </div>
-    </div>
-  );
+			<div>
+				<Row className="link1">
+					<Col
+						lg={{ offset: 10 }}
+						md={{ offset: 8 }}
+						sm={{ offset: 5 }}
+						xs={{ offset: 5 }}>
+						<a
+							className="vm1"
+							href="https://hatecrimetracker.1thing.org/"
+							target="_blank"
+							rel="noopener noreferrer">
+							View More &rarr;
+						</a>
+					</Col>
+				</Row>
+			</div>
+		</div>
+	);
 };
 
 export default ChartView;
