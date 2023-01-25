eff8501915d0f5888b428f2cec46b8a8617941a9
mzhao99 <zhao.man@northeastern.edu>
2023-01-25 14:59:22 -0500

    established basic structure

---
+++ src/components/home/Home.tsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom/client';
+
+class Home extends Component{
+    render() {
+        return (
+            <div>
+                <h1>1 Thing Org</h1>
+            </div>
+        )
+    }
+}
+export default Home;
