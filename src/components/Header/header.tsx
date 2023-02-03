b2ef12ccf4f41db69b646da9ba4b988ad6b053ac
httplife <me@httplife.com>
2023-02-02 16:21:06 -0800

    remove heroicon, add react-icons, update footer with new icons

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -1,7 +1,7 @@
 import { Popover, Transition } from "@headlessui/react"
-import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline"
 import { Fragment } from "react"
-
+import { GiHamburgerMenu } from "react-icons/gi"
+import { GrClose } from "react-icons/gr"
 import { Link } from "react-router-dom"
 import Logo from "../../assets/images/logo.svg"
 
@@ -27,7 +27,7 @@ export default function HeaderSection() {
           <div className="-my-2 -mr-2 md:hidden">
             <Popover.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
               <span className="sr-only">Open menu</span>
-              <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+              <GiHamburgerMenu className="h-6 w-6" aria-hidden="true" />
             </Popover.Button>
           </div>
 
@@ -59,7 +59,7 @@ export default function HeaderSection() {
                 <div className="-mr-2">
                   <Popover.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
                     <span className="sr-only">Close menu</span>
-                    <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+                    <GrClose className="h-6 w-6" aria-hidden="true" />
                   </Popover.Button>
                 </div>
               </div>
--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,7 +1,10 @@
 import { Component } from "react"
+import { AiOutlineMail } from "react-icons/ai"
+import { ImGithub } from "react-icons/im"
+import { TiSocialFacebook, TiSocialLinkedin, TiSocialTwitter } from "react-icons/ti"
+import { Link } from "react-router-dom"
 import "./footer.css"
 
-import { Link } from "react-router-dom"
 class Footer extends Component {
   render() {
     return (
@@ -10,15 +13,26 @@ class Footer extends Component {
           <div className="left-side">
             <h4>1 Thing Against Racism</h4>
             <p>
-              1 Thing Against Racism is a grassroots, non-profit organization of volunteers founded in [DATE]. We
-              believe ending racism and hate begins with uniting people through compassion and advocacy. Together we can
-              make the world a more beautiful place — one thing at a time.
+              1 Thing Against Racism is a grassroots, non-profit organization of volunteers founded in 2022. We believe
+              ending racism and hate begins with uniting people through compassion and advocacy. Together we can make
+              the world a more beautiful place — one thing at a time.
             </p>
-            <div className="social-icons">
-              <Link to="https://twitter.com/1Thing_Org">Email</Link>
-              <Link to="https://twitter.com/1Thing_Org">Twitter</Link>
-              <Link to="https://www.linkedin.com/company/1-thing-org">Linkedin</Link>
-              <Link to="https://www.facebook.com/people/One-Thing-Org/100081311228444">Facebook</Link>
+            <div className="social-icons flex space-x-4 text-xl">
+              <Link target="_self" to="#" onClick={() => ((window as Window).location = "mailto:info@1thing.org")}>
+                <AiOutlineMail />
+              </Link>
+              <Link to="https://twitter.com/1Thing_Org">
+                <TiSocialTwitter />
+              </Link>
+              <Link to="https://www.linkedin.com/company/1-thing-org">
+                <TiSocialLinkedin />
+              </Link>
+              <Link to="https://www.facebook.com/people/One-Thing-Org/100081311228444">
+                <TiSocialFacebook />
+              </Link>
+              <Link to="https://github.com/1thing-org">
+                <ImGithub />
+              </Link>
             </div>
           </div>
           <div className="split-line"></div>
