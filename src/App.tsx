2a857ddeef512d7f43da7860289f1d8baee4927c
JahongLiu <jahongliu@gmail.com>
2023-06-20 11:18:32 -0400

    add routes for projects details

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
-import './App.css';
-import Home from './components/home/Home';
-import TakeAction from './components/actions/TakeAction';
-import Testimonials from './components/testimonials/Testimonials';
-import AboutUs from './components/about/AboutUs';
-import Contact from './components/contact/Contact';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./components/home/Home";
+import TakeAction from "./components/actions/TakeAction";
+import Testimonials from "./components/testimonials/Testimonials";
+import AboutUs from "./components/about/AboutUs";
+import Contact from "./components/contact/Contact";
+import ProjectDetails from "./components/projectDetails/ProjectDetails";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           <Route path="/testimonials" element={<Testimonials />} />
           <Route path="/aboutus" element={<AboutUs />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path="/projects/:id" element={<ProjectDetails />} />
         </Routes>
       </Router>
     </div>
