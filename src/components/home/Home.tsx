ee23845b117a6c88d08673cc5b58dcc7938dae1e
Jahong Liu <121676539+JahongLiu@users.noreply.github.com>
2023-06-21 16:18:01 -0400

    V2 (#41)
    * bring back the TESTIMONIALS
    
    * add routes for projects details
    
    * keep testimonial commented out for now

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,31 +1,34 @@
-import NavBar from '../navbar/NavBar';
-import Footer from '../footer/Footer';
-import HeroSection from '../hero/HeroSection';
-import ProjectSection from './ProjectSection/ProjectSection';
-import StorySection from './StorySection/StorySection';
-import ActionSection from './ActionSection/ActionSection';
-import WhoWeAre from 'components/whoWeAre/WhoWeAre';
-import WorkingOn from './WorkingOn/WorkingOn';
-import ChartView from './Chart/ChartView';
+import NavBar from "../navbar/NavBar";
+import Footer from "../footer/Footer";
+import HeroSection from "../hero/HeroSection";
+import ProjectSection from "./ProjectSection/ProjectSection";
+import StorySection from "./StorySection/StorySection";
+import ActionSection from "./ActionSection/ActionSection";
+import WhoWeAre from "components/whoWeAre/WhoWeAre";
+import WorkingOn from "./WorkingOn/WorkingOn";
+import ChartView from "./Chart/ChartView";
 
-const homeHeroData: {heading: string, subheading: string} = {
-    "heading": `Small Actions can add up to improve racial equality`,
-    "subheading": `Take action. Share your story. Inspire others.`
-}
+const homeHeroData: { heading: string; subheading: string } = {
+  heading: `Small Actions can add up to improve racial equality`,
+  subheading: `Take action. Share your story. Inspire others.`,
+};
 
-function Home(){
-    return (
-        <div>
-            <NavBar />
-            <HeroSection heading={homeHeroData.heading} subheading={homeHeroData.subheading} />
-            <WhoWeAre/>
-            <ChartView/>
-            {/* <ProjectSection /> */}
-            <WorkingOn/> 
-            <StorySection />
-            <ActionSection />
-            <Footer />
-        </div>
-    )
+function Home() {
+  return (
+    <div>
+      <NavBar />
+      <HeroSection
+        heading={homeHeroData.heading}
+        subheading={homeHeroData.subheading}
+      />
+      <WhoWeAre />
+      <ChartView />
+      {/* <ProjectSection /> */}
+      <WorkingOn />
+      <StorySection />
+      <ActionSection />
+      <Footer />
+    </div>
+  );
 }
 export default Home;
