1048cec96cd5c2b6664690deaa577cdcd6da96b9
rayhm6419 <105839955+rayhm6419@users.noreply.github.com>
2023-06-24 15:34:05 -0700

    Updated TopSection and Tracker Section (#42)
    * Update README.md
    
    * section updated
    
    * updated
    
    * most updated
    
    * Dimensions Responsive updated
    
    * Dimensions Responsive updated
    
    * css the wrapper for graph
    removed the daily count and monthly count tags
    removed the bar for daily count
    changed the color of the bar for monthly count
    css complected for the graph, starting from 1024px to any large screen size
    added the css for the graph to be responsive
    
    * center the text in the who we are section
    
    * NavBar text font adjustment in NavBar.css
    Chart color adjustment in IncidentChart.js
    
    * Modified responding to the feedback:
    1.The font is match now
    2.logo background removed in navbar.css
    3.Take Action in one line in navbar.css
    4.Chart datapoints color and missing months were fixed in IncidentChart.css
    5.Abbreviation changed in IncidentChart.css. e.g: Jan instead of January
    6.One of the project section temporary displayed from home page in Home.tsx
    
    * New updates:
    
    HeroSection match design now (font, yellow box position, and image height)
    Font for the buttons matched design
    When screen is small, compoemts align now.
    
    * Chart tooptip fixed
    
    Include the i18n related files to show proper english text.
    
    * change min and max width to 1440px
    
    * change min and max width to 1440px
    
    * implemented grid system from react-bootsrapt
    
    screen size responsive:
    Navbar -from size 768px to 1440px max
    HeroSection -from size 768px to 1440px max
    
    WorkingOn Section added back to the page
    
    * Screen Responsive:
    WhoWEAre Section: responsive from min 768 to max 1440px
                      background color changed
    Hate Crime Tracker: background color changed
                        font color changed
                        responsive from min 768 to max 1440px
    
    * Working on the top section and tracker
    
    implement the toggle on navBar for mobile screen size
    
    changed the slogan on HeroSection
    
    implement responsive screen size from 500px - 768px for:
    
    TopSection
    Tracker section
    
    ---------
    
    Co-authored-by: Js <me@httplife.com>
    Co-authored-by: Li <lima01@gmail.com>

--- src/components/home/WorkingOn/WorkingOn.tsx
+++ src/components/home/WorkingOn/WorkingOn.tsx
@@ -1,36 +1,26 @@
-import React from "react";
-import "./WorkingOn.css";
-import placeholderimg from "../../../assets//home-page/working-on-placeholder.svg";
+import React from 'react';
+import './WorkingOn.css';
+import placeholderimg from '../../../assets//home-page/working-on-placeholder.svg';
 
 function WorkingOn() {
   return (
-    <div className="working-on-container">
-      <h1 className="main-title">We're also Working on</h1>
-      <div className="images">
-        <div className="image-container">
-          <img className="left-image" src={placeholderimg} alt="Left Image" />
-          <h2 className="title">Project Together</h2>
-          <p className="working-text">
-            Lorem ipsum dolor sit amet consectetur. Aliquam sed egestas enim
-            porttitor tristique id lectus. Eget orci vestibulum fringilla diam
-            adipiscing proin.{" "}
-          </p>
-          <a href="/projects/1">Learn More</a>
-        </div>
-        <div className="image-container">
-          <img className="right-image" src={placeholderimg} alt="Right Image" />
-          <h2 className="title">Supporting Victims</h2>
-          <p className="working-text">
-            Lorem ipsum dolor sit amet consectetur. Aliquam sed egestas enim
-            porttitor tristique id lectus. Eget orci vestibulum fringilla diam
-            adipiscing proin.{" "}
-          </p>
-          <a className="working-link" href="/projects/2">
-            Learn More
-          </a>
-        </div>
-      </div>
+<div className="working-on-container">
+  <h1 className="main-title">We're also Working on</h1>
+  <div className="images">
+    <div className="image-container">
+      <img className="left-image" src={placeholderimg} alt="Left Image" />
+      <h2 className='title'>Project Together</h2>
+      <p className='working-text'>Lorem ipsum dolor sit amet consectetur. Aliquam sed egestas enim porttitor tristique id lectus. Eget orci vestibulum fringilla diam adipiscing proin. </p>
+      <a href="/" >Learn More</a>
     </div>
+    <div className="image-container">
+      <img className="right-image" src={placeholderimg} alt="Right Image" />
+      <h2 className='title'>Supporting Victims</h2>
+      <p className='working-text'>Lorem ipsum dolor sit amet consectetur. Aliquam sed egestas enim porttitor tristique id lectus. Eget orci vestibulum fringilla diam adipiscing proin. </p>
+      <a className='working-link'  href="/" >Learn More</a>
+    </div>
+  </div>
+</div>
   );
 }
 export default WorkingOn;
