f6046a90ddd8f3dfee5df9a94be247bf6a89ccc5
JahongLiu <jahongliu@gmail.com>
2023-06-24 16:08:50 -0400

    update the HeroSection image for take action page

--- src/components/actions/TakeAction.tsx
+++ src/components/actions/TakeAction.tsx
@@ -1,25 +1,26 @@
-import NavBar from '../navbar/NavBar';
-import Footer from '../footer/Footer';
-import ShareOneThing from './share/ShareOneThing';
-import ActionsGroup from './actionsgroup/ActionsGroup';
-import HeroSection from '../hero/HeroSection';
+import NavBar from "../navbar/NavBar";
+import Footer from "../footer/Footer";
+import ShareOneThing from "./share/ShareOneThing";
+import ActionsGroup from "./actionsgroup/ActionsGroup";
+import HeroSection from "../hero/HeroSection";
+import HeroActionImage from "assets/hero-action.svg";
 
-const actionHeroData: {heading: string, subheading: string} = {
-    "heading": `There are many small actions we can take to improve racial equality.`,
-    "subheading": `Take small actions consistently. Achieve racial equality eventually
-                  Get inspiration about what you can do to help.`
-}
+const actionHeroData: { heading: string; subheading: string } = {
+  heading: `There are many small actions we can take to improve racial equality.`,
+  subheading: `Take small actions consistently. Achieve racial equality eventually
+                  Get inspiration about what you can do to help.`,
+};
 
-function TakeAction(){
-    return (
-        <div>
-            <NavBar />
-            <HeroSection heading={actionHeroData.heading} subheading={actionHeroData.subheading} />
-            <ActionsGroup/>
-            <ShareOneThing/>
-            <Footer />
-        </div>
-    )
+function TakeAction() {
+  return (
+    <div>
+      <NavBar />
+      <HeroSection imageSrc={HeroActionImage} />
+      <ActionsGroup />
+      <ShareOneThing />
+      <Footer />
+    </div>
+  );
 }
 
 export default TakeAction;
