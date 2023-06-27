37c4a1420661f3675e66195ddb77f6220658379a
JahongLiu <jahongliu@gmail.com>
2023-06-27 10:44:12 -0400

    use simple way for projects deails instead of the dynamic way

---
+++ src/components/projectDetails/ProjectTogether.tsx
@@ -0,0 +1,25 @@
+import NavBar from "../navbar/NavBar";
+import Footer from "../footer/Footer";
+import HeroSection from "../hero/HeroSection";
+import HeroImage from "assets/hero.svg";
+
+const aboutHeroData: { heading: string; subheading: string } = {
+  heading: `Project Together Page`,
+  subheading: `Project Together is a mobile app that connects people who want to support the AAPI community.`,
+};
+
+function ProjectTogether() {
+  return (
+    <div>
+      <NavBar />
+      <HeroSection
+        heading={aboutHeroData.heading}
+        subheading={aboutHeroData.subheading}
+        imageSrc={HeroImage}
+      />
+      <Footer />
+    </div>
+  );
+}
+
+export default ProjectTogether;
---
+++ src/components/projectDetails/VictimSupportFund.tsx
@@ -0,0 +1,25 @@
+import NavBar from "../navbar/NavBar";
+import Footer from "../footer/Footer";
+import HeroSection from "../hero/HeroSection";
+import HeroImage from "assets/hero.svg";
+
+const aboutHeroData: { heading: string; subheading: string } = {
+  heading: `Victim Support Fund Page`,
+  subheading: `1thing.org receives donations from individuals and corporations like Google. We disburse the funds to victims of anti-Asian hate crimes in a transparent and accountable manner, and provide regular updates to donors and the public on the progress of the fundraising campaign and how the funds are being used. With careful planning and execution, we ensure that the funds are used effectively and efficiently. We ensure that victims receive support and resources to address their needs and challenges.`,
+};
+
+function VictimSupportFund() {
+  return (
+    <div>
+      <NavBar />
+      <HeroSection
+        heading={aboutHeroData.heading}
+        subheading={aboutHeroData.subheading}
+        imageSrc={HeroImage}
+      />
+      <Footer />
+    </div>
+  );
+}
+
+export default VictimSupportFund;
