1d207e61dea31f23812c1fc8fad238a08b384843
Steph <stepskycc@gmail.com>
2023-07-07 11:20:18 -0400

    Added Mailchimp & corresponding CSS code

---
+++ src/utility/mailchimp.js
@@ -0,0 +1,30 @@
+import { Component } from "react";
+import Mailchimp from "react-mailchimp-form";
+
+class MailChimpForm extends Component {
+  render() {
+    return (
+      <Mailchimp
+        action="https://1thing.us20.list-manage.com/subscribe/post?u=21d473c726961a9ff53d21a35&id=81aac402d9"
+        fields={[
+          {
+            name: "EMAIL",
+            placeholder: "Email",
+            type: "email",
+            required: true
+          }
+        ]}
+        messages={{
+          sending: "Sending...",
+          success: "Thank you for subscribing!",
+          error: "An unexpected internal error has occurred.",
+          empty: "Please provide a valid e-mail.",
+          duplicate: "Too many subscribe attempts for this email address",
+          button: "Subscribe!"
+        }}
+      />
+    )
+  }
+}
+
+export default MailChimpForm
