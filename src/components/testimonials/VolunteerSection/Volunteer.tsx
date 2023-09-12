7c1292d644984fc3c5d5278a59c53515dfd959e0
Chelseaguan0219 <81730540+Chelseaguan0219@users.noreply.github.com>
2023-09-12 16:54:56 -0700

    Add Carousel to testimonial homepage (#78)

--- src/components/testimonials/VolunteerSection/Volunteer.tsx
+++ src/components/testimonials/VolunteerSection/Volunteer.tsx
@@ -1,5 +1,9 @@
 import Card from 'react-bootstrap/Card';
 import "./Volunteer.css";
+import "./Volunteer.css";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; 
+
+
 
 type Props = {
     name: string;
@@ -9,12 +13,12 @@ type Props = {
 }
 
 function Volunteer(props: Props){
+      
     return (
         <Card id="volunteer">
             <Card.Body id="left-side">
                 <Card.Text id="volunteer-quote">{props.quote}</Card.Text>
                 <Card.Link href={`/interviews/${props.name}/${encodeURIComponent(props.interview)}/${encodeURIComponent(props.pic)}`} id="volunteer-interview">Read the testimonial</Card.Link>
-
             </Card.Body>
             <Card.Body id="right-side">
             <Card.Img id="volunteer-profile-pic" src={props.pic} alt="Profile" />
