6b45972061b4b4c9d86e6a48b880cf2bae6ddd19
rayhm6419 <haoml6419@gmail.com>
2023-09-22 15:06:12 -0700

    Remove Unrelated Text

---
+++ src/components/testimonials/VolunteerSection/Interview.tsx
@@ -0,0 +1,47 @@
+
+
+// src/InterviewAnswers.js
+import React from 'react';
+
+const Interview = () => {
+  return (
+    <div>
+      <h1>Interview Answers</h1>
+      {/* Add your interview answers content here */}
+      <p className='interview'>
+        In April 2021, my journey to 1 Thing started with a basic presentation
+        to document anti-Asian hate incidents, aiming to raise awareness among
+        Google colleagues about the AAPI community's challenges. Unexpectedly,
+        the presentation garnered attention within Google, leading others to
+        contribute additional incidents, refine content, and share it widely. As
+        a result, the presentation reached thousands of Googlers and was cited
+        in numerous internal events and initiatives. I noticed that many AAPI
+        community members, including myself, were upset about anti-Asian hate
+        but seldom took action. Upon reflecting on my own inaction, I identified
+        several excuses, such as feeling powerless, lacking time and resources,
+        struggling to find suitable actions, being discouraged by the lack of
+        results, and feeling isolated due to others' inactivity. The
+        presentation experience shifted my perspective, motivating me to take
+        manageable actions without fretting over immediate results and
+        encouraging others to join me. I believed that if enough people became
+        mobilized, real change would follow. This led me to establish the
+        non-profit organization, 1 Thing Against Racism. 1 Thing's mission is to
+        mobilize individuals and communities to take small actions against
+        racism and hate, inspiring others to participate in the movement. By
+        fostering a cycle of action, sharing, and inspiration, we aim to create
+        a positive feedback loop that combines small efforts into a large social
+        movement driving meaningful change. Using our volunteers' technological
+        expertise, 1 Thing developed tools to help others embrace the 1 Thing
+        concept. We transformed the anti-Asian hate incident presentation into
+        the real-time tracking website hatecrimetracker.1thing.org. Upon
+        analyzing incident data, we found many victims and families needed
+        immediate assistance, so we established the anti-Asian hate victim
+        support fund. Furthermore, volunteers are developing Together, a mobile
+        app tailored for social and racial equality volunteers, enabling them to
+        practice the 1 Thing idea and achieve their objectives.
+      </p>
+    </div>
+  );
+};
+
+export default Interview;
