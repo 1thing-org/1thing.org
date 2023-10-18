b24f90608c8d303049e388985078616e623ccf8f
Chelseaguan0219 <81730540+Chelseaguan0219@users.noreply.github.com>
2023-10-18 12:05:18 -0700

    update out-story homepage (#104)
    * Add Carousel to testimonial homepage
    
    * adjust css for story section
    
    * update for pr87
    
    * update 'out-story' homepage

--- src/components/home/StorySection/StorySection.tsx
+++ src/components/home/StorySection/StorySection.tsx
@@ -1,6 +1,7 @@
 import Story from "./Story";
 import Card from "react-bootstrap/Card";
-import "./StorySection-2.css";
+// import "./StorySection-2.css";
+import "./Story.css";
 import { Button } from "react-bootstrap";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
@@ -26,49 +27,49 @@ const storyData: { name: string; quote: string; picUrl: string }[] = [
       "Share your story, because how else are you going to get to know another person? Be empathetic. You have to start somewhere and what easier place to start than with your personal story?",
     picUrl: require("assets/volunteers/Josh.png"),
   },
-  {
-    name: "Hannelore B.",
-    quote:
-      "Lorem ipsum dolor sit amet consectetur. Aliquam sed egestas enim porttitor tristique id lectus.",
-    picUrl: require("assets/home-page/profile-placeholder.jpeg"),
-  },
 ];
 
 function StorySection() {
-  const [isMobile, setIsMobile] = useState(window.innerWidth < 896);
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 700);
 
   useEffect(() => {
     window.addEventListener("resize", () => {
-      setIsMobile(window.innerWidth < 896);
+      setIsMobile(window.innerWidth < 700);
     });
   }, []);
   return (
     <div id="story-section">
-      
-      <Container id="story-desc">
-      <Col style={{display:"flex", justifyContent:"center", alignItems:"center", gap:"8px"}}>
-      <img src={logo} style={{width:'37px', height: '23.5px'}}></img>
-        <h2 className="our-story">Our Story</h2>
+    
+        <Row className="mb-2">
+          <Col style={{display:"flex", justifyContent:"center", alignItems:"center", gap:"8px"}}>
+            <img src={logo} style={{width:'37px', height: '23.5px'}}></img>
+          </Col>
+          <Col>
+            <h2 className="our-story">Our Story</h2>
+          </Col>
+        </Row>
+        <Col className="mt-1 mb-2" >
+          <p className="story-text">
+            1 Thing Against Racism began as a slide deck shared around the offices
+            at Google. Today, it is a mission driven organization comprised of
+            volunteers who contribute their time to our mission.
+          </p>
         </Col>
-        <p className="story-text">
-        
-          1 Thing Against Racism began as a slide deck shared around the offices
-          at Google. Today, it is a mission driven organization comprised of
-          volunteers who contribute their time to our mission.
-        </p>
-        <Button className="story-button" href="#">
-          Read More
-        </Button>
-      </Container>
-
+         
+      
+        <Row>
       {isMobile ? (
+       
         <Carousel
           showThumbs={false}
           showStatus={false}
           dynamicHeight={false}
           useKeyboardArrows={true}
         >
+      
+        <Col className="mt-0 mb-0">
           <Card id="story">
+            <Card.Text id="story-name" className="story-name">Li M.</Card.Text>
             <Card.Text id="story-quote">
             The presentation experience shifted my perspective, motivating me to take manageable actions without fretting over immediate results and encouraging others to join me. I believed that if enough people became mobilized, real change would follow. This led me to establish the non-profit organization, 1 Thing Against Racism.
             </Card.Text>
@@ -76,11 +77,13 @@ function StorySection() {
               <Card.Img
                 id="profile-pic"
                 src={require("assets/volunteers/Li.png")}
-              />
-              <Card.Text id="story-name">Li M.</Card.Text>
+              />   
             </Card.Body>
           </Card>
+        </Col>
+        <Col className="mt-0 mb-0">
           <Card id="story">
+            <Card.Text id="story-name" className="story-name">Kai K.</Card.Text>
             <Card.Text id="story-quote">
             Sometimes we can be daunted by how big of an idea we have but once we start breaking it down and just start doing something, no matter how big or small it is at least we are doing something. The big idea will feel less daunting and more achievable.
             </Card.Text>
@@ -88,11 +91,13 @@ function StorySection() {
               <Card.Img
                 id="profile-pic"
                 src={require("assets/home-page/profile-placeholder.jpeg")}
-              />
-              <Card.Text id="story-name">Kai K.</Card.Text>
+              />  
             </Card.Body>
           </Card>
+        </Col>
+        <Col className="mt-0 mb-0">
           <Card id="story">
+            <Card.Text id="story-name" className="story-name">Josh C.</Card.Text>
             <Card.Text id="story-quote">
             Share your story, because how else are you going to get to know another person? Be empathetic. You have to start somewhere and what easier place to start than with your personal story?
             </Card.Text>
@@ -101,23 +106,12 @@ function StorySection() {
                 id="profile-pic"
                 src={require("assets/volunteers/Josh.png")}
               />
-              <Card.Text id="story-name">Josh C.</Card.Text>
+              
             </Card.Body>
           </Card>
-          <Card id="story">
-            <Card.Text id="story-quote">
-              Lorem ipsum dolor sit amet consectetur. Aliquam sed egestas enim
-              porttitor tristique id lectus.
-            </Card.Text>
-            <Card.Body id="story-body">
-              <Card.Img
-                id="profile-pic"
-                src={require("assets/home-page/profile-placeholder.jpeg")}
-              />
-              <Card.Text id="story-name">Hannelore B.</Card.Text>
-            </Card.Body>
-          </Card>
+        </Col>
         </Carousel>
+     
       ) : (
         <div id="stories">
           {storyData.map((story) => (
@@ -127,11 +121,19 @@ function StorySection() {
               quote={story.quote}
               picUrl={story.picUrl}
             />
+            
           
           ))}
         </div>
       )}
+         </Row> 
+      <Row>
+         <Button className="story-button" href="#">
+          Read More
+        </Button>
+      </Row>
     </div>
+    
   );
 }
 
