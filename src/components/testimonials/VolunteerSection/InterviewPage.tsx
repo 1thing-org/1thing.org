adcde9d100dfe68923ef18eb09b0e15c8fb8bbe4
jiayingxie <xie.jiay@northeastern.edu>
2023-10-22 23:55:11 -0700

    Format InterviewPage.

--- src/components/testimonials/VolunteerSection/InterviewPage.tsx
+++ src/components/testimonials/VolunteerSection/InterviewPage.tsx
@@ -1,25 +1,38 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import React from "react";
+import { useParams } from "react-router-dom";
 import "./InterviewPage.css";
 
 function InterviewPage() {
-    const { name, pic,interview } = useParams<{ name?: string; quote?: string; pic?: string; interview?:string }>();
-    const paragraphs = interview ? decodeURIComponent(interview).split('\n\n') : [];
+  const { name, pic, interview } = useParams<{
+    name?: string;
+    quote?: string;
+    pic?: string;
+    interview?: string;
+  }>();
+  const paragraphs = interview
+    ? decodeURIComponent(interview).split("\n\n")
+    : [];
 
-    if (!name  || !pic || !interview) {
-      // Handle the case when name or quote is undefined
-      return <div>Invalid URL parameters.</div>;
-    }
-  
-    return (
-      <div>
-        <h1 className='interview-title'>{name}</h1>
-        <img className = 'interview-profile'src={decodeURIComponent(pic)} alt={`${name}'s pic`} />
-        {paragraphs.map((para, index) => (
-        <p className='interview-paragraph'key={index}>{para}</p>
-      ))}
-        {/* Display the interview content here */}
-      </div>
-    );
+  if (!name || !pic || !interview) {
+    // Handle the case when name or quote is undefined
+    return <div>Invalid URL parameters.</div>;
   }
+
+  return (
+    <div>
+      <h1 className="interview-title">{name}</h1>
+      <img
+        className="interview-profile"
+        src={decodeURIComponent(pic)}
+        alt={`${name}'s pic`}
+      />
+      {paragraphs.map((para, index) => (
+        <p className="interview-paragraph" key={index}>
+          {para}
+        </p>
+      ))}
+      {/* Display the interview content here */}
+    </div>
+  );
+}
 export default InterviewPage;
