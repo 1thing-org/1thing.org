43c35ab45c56bbff89dbe7798c9533f3162cbd5e
rayhm6419 <105839955+rayhm6419@users.noreply.github.com>
2024-02-27 18:15:48 -0800

    Updated Content (#207)

--- src/components/about/AboutUsContent.tsx
+++ src/components/about/AboutUsContent.tsx
@@ -10,29 +10,26 @@ const aboutUsData: {
   name: string;
   desc: string;
 }[] = [
-    {
-      name: "About Us",
-      desc:
-        "1 Thing Organization, a non-profit dedicated to combating Anti-Asian Hate, was founded by Li Ma, a Technical Lead Manager at Google. Recognizing the urgent need to address this social issue, Li Ma created the Anti-Asian Hate Crime Tracker website to document cases and analyze hate crime trends, spreading awareness far and wide. What began as a small team of two has now flourished into a dedicated group of 20 individuals, and the organization is officially registered as a company. This growth aligns seamlessly with the company's ethos and objectives: advocating for small contributions that collectively enhance racial equality.",
-    },
-    {
-      name: "Our Mission",
-      desc:
-        "To mobilize individuals and communities to take small actions against racism and hate, and to inspire others to join the movement. Through a chain of action, share, and inspire, we aim to create a positive feedback loop that aggregates small efforts into a large social movement that can make real differences in our society.",
-    },
-    {
-      name: "Our Vision",
-      desc:
-        "Our vision is a world where everyone is treated with respect and dignity, regardless of their race, ethnicity, or background. We advocate the 1 Thing idea and use our members' technical expertise to create a platform that supports individuals and communities to easily exercise the 1 Thing idea. We believe that by working together, we can create a more just and equitable society for all.",
-    }
-  ];
+  {
+    name: "About Us",
+    desc: "1 Thing, a non-profit dedicated to combating Anti-Asian Hate, was founded by Li Ma, a Technical Lead Manager at Google. Recognizing the urgent need to address this social issue, Li Ma created the Anti-Asian Hate Crime Tracker to document cases and analyze hate crime trends, spreading awareness far and wide. What began as a small team of two has now grown into a dedicated group of 20 individuals, and the organization is officially registered as a non-profit company. The organization advocates for small contributions that collectively enhance racial equality.",
+  },
+  {
+    name: "Our Mission",
+    desc: "To mobilize individuals and communities to take small actions against racism and hate, and to inspire others to join the movement. Through a chain of action, sharing, and inspiration, we aim to create a positive feedback loop that aggregates small efforts into a large social movement that can make real differences in our society.",
+  },
+  {
+    name: "Our Vision",
+    desc: "Our vision is a world where everyone is treated with respect and dignity, regardless of their race, ethnicity, or background. We advocate the 1 Thing idea and use our members' expertise to create a platform that supports individuals and communities to easily implement the 1 Thing idea. We believe that by working together, we can create a more just and equitable society for all.",
+  },
+];
 
 function AboutUsSection() {
   return (
     <Container id="about-us-container">
       <NavBar />
       <img className="heart"
-        src={heartImg}
+        // src={}
         style={{
           borderRadius: '0',
          
