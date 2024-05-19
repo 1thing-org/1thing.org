4c9a8bc59acb543f6398e645d5d01ef19a7dd535
jiayingxie <xie.jiay@northeastern.edu>
2024-05-18 21:12:48 -0700

    Format files and remove unused code.

--- src/components/projects/projectTogether/bottomSection/BottomSec.tsx
+++ src/components/projects/projectTogether/bottomSection/BottomSec.tsx
@@ -1,47 +1,40 @@
 import smallLogo from "../../../../assets/home-page/smallLogo.svg";
-import { Button, Container, Image, Col, Row, Card } from "react-bootstrap";
-import './BottomSec.css';
-import Bottom from './Bottom';
+import { Container, Image, Col, Row, Card } from "react-bootstrap";
+import "./BottomSec.css";
+import Bottom from "./Bottom";
 import a1 from "../../../../assets/home-page/a1.svg";
 import a5 from "../../../../assets/home-page/a5.svg";
 import a3 from "../../../../assets/home-page/a3.svg";
 import a4 from "../../../../assets/home-page/a4.svg";
 const FeatureData: { name: string; quote: string }[] = [
   {
-
     name: "COMMUNITY",
     quote:
-      'Find a community of people who share their passion for supporting the AAPI community.',
+      "Find a community of people who share their passion for supporting the AAPI community.",
   },
   {
-
     name: "EAST NAVIGATION",
-    quote:
-      'Easily find and join projects that are already underway.',
+    quote: "Easily find and join projects that are already underway.",
   },
   {
-
     name: "INITIATE",
     quote:
-      'Propose their own projects and find volunteers to help them implement them.',
+      "Propose their own projects and find volunteers to help them implement them.",
   },
   {
-
     name: "TRACK PROGRESS",
     quote:
-      'Be able to see how much progress is being made on each project, and how the project is impacting the AAPI community. ',
+      "Be able to see how much progress is being made on each project, and how the project is impacting the AAPI community. ",
   },
   {
-
     name: "VOLUNTEER CONNECTION",
     quote:
-      'It would be great to be able to connect with other volunteers who are working on similar projects. This could lead to collaboration and new ideas.',
+      "It would be great to be able to connect with other volunteers who are working on similar projects. This could lead to collaboration and new ideas.",
   },
   {
-
     name: "SHARE EXPERIENCE",
     quote:
-      'It would be great to be able to share stories and experiences of volunteering. This would help to inspire others and build a sense of community.',
+      "It would be great to be able to share stories and experiences of volunteering. This would help to inspire others and build a sense of community.",
   },
 ];
 function BottomSec() {
@@ -49,18 +42,19 @@ function BottomSec() {
     <div id="bottom-sections">
       <div id="core-features-section">
         <Container id="core-features-container">
-          <img className="feature-logo" src={smallLogo} style={{ width: "37px", height: "23.5px" }} alt="1thing logo"></img>
+          <img
+            className="feature-logo"
+            src={smallLogo}
+            style={{ width: "37px", height: "23.5px" }}
+            alt="1thing logo"
+          ></img>
           <h2 className="core-features">Core Features</h2>
         </Container>
         <Container id="features">
-          <Row >
-
+          <Row>
             {FeatureData.map((feature, index) => (
-              <Col md={6} key={index} style={{ padding: '0' }}>
-                <Bottom
-                  name={feature.name}
-                  quote={feature.quote}
-                />
+              <Col md={6} key={index} style={{ padding: "0" }}>
+                <Bottom name={feature.name} quote={feature.quote} />
               </Col>
             ))}
           </Row>
@@ -70,12 +64,18 @@ function BottomSec() {
       <div id="lower">
         <div id="core-features-lower-section">
           <Container id="core-features-container">
-            <img className="feature-logo" src={smallLogo} style={{ width: "37px", height: "23.5px" }} alt="1thing logo"></img>
+            <img
+              className="feature-logo"
+              src={smallLogo}
+              style={{ width: "37px", height: "23.5px" }}
+              alt="1thing logo"
+            ></img>
             <h2 className="core-features">What We Can Bring</h2>
           </Container>
           <p className="bottom-text">
-            There are many challenges that prevent the AAPI community and our allies from taking concrete actions to fight against
-            the form of hate and discrimination we are facing:
+            There are many challenges that prevent the AAPI community and our
+            allies from taking concrete actions to fight against the form of
+            hate and discrimination we are facing:
           </p>
           <Row className="icon-row">
             <Col md={3} id="icon">
@@ -99,23 +99,9 @@ function BottomSec() {
       </div>
 
       <div id="support">
-        <div id="core-features-support-section">
-          {/* <Container id="core-features-container">
-            <img className="feature-logo" src={smallLogo} style={{ width: "37px", height: "23.5px" }} alt="1thing logo"></img>
-            <h2 className="core-features">We appreciate your support!</h2>
-          </Container>
-
-          <Col xs={12} className="bottom-text">
-            Project Together has the potential to make a real difference in the AAPI community. It will help to connect people
-            who want to support the AAPI community and make a difference, and it will make it easier for people to find ways to help.
-          </Col> */}
-
-          {/* <Button id="donate-button">Donate</Button> */}
-        </div>
+        <div id="core-features-support-section"></div>
       </div>
     </div>
-
-
   );
 }
 
