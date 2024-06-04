000fbb14ca37a26bc473c936f67f6899fa3619e9
Jiaying Xie <xie.jiay@northeastern.edu>
2024-06-04 07:46:19 -0700

    Format files and remove unused code. (#227)

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -1,30 +1,29 @@
-import NavBar from '../navbar/NavBar';
-import Footer from '../footer/Footer';
-import ContactSection from './ContactSection';
-import SignUpForm from 'utility/signup';
+import NavBar from "../navbar/NavBar";
+import Footer from "../footer/Footer";
+import ContactSection from "./ContactSection";
 import HeroImage from "assets/hero.svg";
 import "./Contact.css";
-import Signup from 'components/signup/SignUp';
+import Signup from "components/signup/SignUp";
 
-const contactHeroData: { heading: string, subheading: string } = {
-    "heading": `Contact Us`,
-    "subheading": `We ensure that victims receive support and resources to address their needs and challenges.`
-}
+const contactHeroData: { heading: string; subheading: string } = {
+  heading: `Contact Us`,
+  subheading: `We ensure that victims receive support and resources to address their needs and challenges.`,
+};
 
 function Contact() {
-    return (
-      <div>
-        <NavBar />
-        <ContactSection
-          heading={contactHeroData.heading}
-          subheading={contactHeroData.subheading}
-          imageSrc={HeroImage}
-          hideHeroText={true}
-        />
-        <Signup signupTitle={"Contact Us"} containsSubject={true} />
-        <Footer />
-      </div>
-    );
+  return (
+    <div>
+      <NavBar />
+      <ContactSection
+        heading={contactHeroData.heading}
+        subheading={contactHeroData.subheading}
+        imageSrc={HeroImage}
+        hideHeroText={true}
+      />
+      <Signup signupTitle={"Contact Us"} containsSubject={true} />
+      <Footer />
+    </div>
+  );
 }
 
 export default Contact;
